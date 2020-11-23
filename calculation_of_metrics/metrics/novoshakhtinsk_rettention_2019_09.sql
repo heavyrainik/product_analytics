93997cf482a68a35adb11683efd839762bbe0ab2
Yaroslav <Yaroslaw.serow@yandex.ru>
2020-11-24 02:06:20 +0300

    Create novoshakhtinsk_rettention_2019_09.sql

---
+++ calculation_of_metrics/metrics/novoshakhtinsk_rettention_2019_09.sql
@@ -0,0 +1,227 @@
+create table fediq_team.novoshakhtinsk_rettention_2019_09 as(with sum_of_dau as(
+with p as (select dau, date_trunc('month', day) as month from fediq_team.novoshakhtinsk_dau_201909)
+select sum(dau) as sum_of_dau, month
+from p
+group by(month))
+    select 1.0*sum_of_dau / mau as rettention, fediq_team.novochahtinsk_mau_201909.month as month from sum_of_dau, fediq_team.novochahtinsk_mau_201909
+where sum_of_dau.month = fediq_team.novochahtinsk_mau_201909.month)
+
+create table fediq_team.novochahtinsk_dolya_ottoka_201909 as (with p as (select distinct(passenger_id) as passenger_id from fediq_team.novochahtinsk_orders_201909
+    where status = 'complete' and dttm < '2018-02-01'),
+    l as(select count(*) as cnt2 from p),
+ t as (select distinct(passenger_id) from fediq_team.novochahtinsk_orders_201909
+    where status = 'complete' and dttm < '2018-02-01'
+except
+select distinct(passenger_id) from fediq_team.novochahtinsk_orders_201909
+    where status = 'complete' and dttm < '2018-03-01' and dttm >= '2018-02-01 00:00:00'),
+j as (select count(*) as cnt1 from t)
+select 1.0 * cnt1 / cnt2 as dolya_ottoka, ('2018-02-01')::timestamp as month from j, l);
+
+insert into fediq_team.novochahtinsk_dolya_ottoka_201909 (with p as (select distinct(passenger_id) as passenger_id from fediq_team.novochahtinsk_orders_201909
+    where status = 'complete' and '2018-02-01' <= dttm and dttm < '2018-03-01'),
+    l as(select count(*) as cnt2 from p),
+ t as (select distinct(passenger_id) from fediq_team.novochahtinsk_orders_201909
+    where status = 'complete' and '2018-02-01' <= dttm and dttm < '2018-03-01'
+except
+select distinct(passenger_id) from fediq_team.novochahtinsk_orders_201909
+    where status = 'complete' and '2018-03-01' <= dttm and dttm < '2018-04-01'),
+j as (select count(*) as cnt1 from t)
+select 1.0 * cnt1 / cnt2 as dolya_ottoka, ('2018-03-01')::timestamp as month from j, l);
+
+insert into fediq_team.novochahtinsk_dolya_ottoka_201909 (with p as (select distinct(passenger_id) as passenger_id from fediq_team.novochahtinsk_orders_201909
+    where status = 'complete' and '2018-03-01' <= dttm and dttm < '2018-04-01'),
+    l as(select count(*) as cnt2 from p),
+ t as (select distinct(passenger_id) from fediq_team.novochahtinsk_orders_201909
+    where status = 'complete' and '2018-03-01' <= dttm and dttm < '2018-04-01'
+except
+select distinct(passenger_id) from fediq_team.novochahtinsk_orders_201909
+    where status = 'complete' and '2018-04-01' <= dttm and dttm < '2018-05-01'),
+j as (select count(*) as cnt1 from t)
+select 1.0 * cnt1 / cnt2 as dolya_ottoka, ('2018-04-01')::timestamp as month from j, l);
+
+insert into fediq_team.novochahtinsk_dolya_ottoka_201909 (with p as (select distinct(passenger_id) as passenger_id from fediq_team.novochahtinsk_orders_201909
+    where status = 'complete' and '2018-04-01' <= dttm and dttm < '2018-05-01'),
+    l as(select count(*) as cnt2 from p),
+ t as (select distinct(passenger_id) from fediq_team.novochahtinsk_orders_201909
+    where status = 'complete' and '2018-04-01' <= dttm and dttm < '2018-05-01'
+except
+select distinct(passenger_id) from fediq_team.novochahtinsk_orders_201909
+    where status = 'complete' and '2018-05-01' <= dttm and dttm < '2018-06-01'),
+j as (select count(*) as cnt1 from t)
+select 1.0 * cnt1 / cnt2 as dolya_ottoka, ('2018-05-01')::timestamp as month from j, l);
+
+insert into fediq_team.novochahtinsk_dolya_ottoka_201909 (with p as (select distinct(passenger_id) as passenger_id from fediq_team.novochahtinsk_orders_201909
+    where status = 'complete' and '2018-05-01' <= dttm and dttm < '2018-06-01'),
+    l as(select count(*) as cnt2 from p),
+ t as (select distinct(passenger_id) from fediq_team.novochahtinsk_orders_201909
+    where status = 'complete' and '2018-05-01' <= dttm and dttm < '2018-06-01'
+except
+select distinct(passenger_id) from fediq_team.novochahtinsk_orders_201909
+    where status = 'complete' and '2018-06-01' <= dttm and dttm < '2018-07-01'),
+j as (select count(*) as cnt1 from t)
+select 1.0 * cnt1 / cnt2 as dolya_ottoka, ('2018-06-01')::timestamp as month from j, l);
+
+insert into fediq_team.novochahtinsk_dolya_ottoka_201909 (with p as (select distinct(passenger_id) as passenger_id from fediq_team.novochahtinsk_orders_201909
+    where status = 'complete' and '2018-06-01' <= dttm and dttm < '2018-07-01'),
+    l as(select count(*) as cnt2 from p),
+ t as (select distinct(passenger_id) from fediq_team.novochahtinsk_orders_201909
+    where status = 'complete' and '2018-06-01' <= dttm and dttm < '2018-07-01'
+except
+select distinct(passenger_id) from fediq_team.novochahtinsk_orders_201909
+    where status = 'complete' and '2018-07-01' <= dttm and dttm < '2018-08-01'),
+j as (select count(*) as cnt1 from t)
+select 1.0 * cnt1 / cnt2 as dolya_ottoka, ('2018-07-01')::timestamp as month from j, l);
+
+insert into fediq_team.novochahtinsk_dolya_ottoka_201909 (with p as (select distinct(passenger_id) as passenger_id from fediq_team.novochahtinsk_orders_201909
+    where status = 'complete' and '2018-07-01' <= dttm and dttm < '2018-08-01'),
+    l as(select count(*) as cnt2 from p),
+ t as (select distinct(passenger_id) from fediq_team.novochahtinsk_orders_201909
+    where status = 'complete' and '2018-07-01' <= dttm and dttm < '2018-08-01'
+except
+select distinct(passenger_id) from fediq_team.novochahtinsk_orders_201909
+    where status = 'complete' and '2018-08-01' <= dttm and dttm < '2018-09-01'),
+j as (select count(*) as cnt1 from t)
+select 1.0 * cnt1 / cnt2 as dolya_ottoka, ('2018-08-01')::timestamp as month from j, l);
+
+insert into fediq_team.novochahtinsk_dolya_ottoka_201909 (with p as (select distinct(passenger_id) as passenger_id from fediq_team.novochahtinsk_orders_201909
+    where status = 'complete' and '2018-08-01' <= dttm and dttm < '2018-09-01'),
+    l as(select count(*) as cnt2 from p),
+ t as (select distinct(passenger_id) from fediq_team.novochahtinsk_orders_201909
+    where status = 'complete' and '2018-08-01' <= dttm and dttm < '2018-09-01'
+except
+select distinct(passenger_id) from fediq_team.novochahtinsk_orders_201909
+    where status = 'complete' and '2018-09-01' <= dttm and dttm < '2018-10-01'),
+j as (select count(*) as cnt1 from t)
+select 1.0 * cnt1 / cnt2 as dolya_ottoka, ('2018-09-01')::timestamp as month from j, l);
+
+insert into fediq_team.novochahtinsk_dolya_ottoka_201909 (with p as (select distinct(passenger_id) as passenger_id from fediq_team.novochahtinsk_orders_201909
+    where status = 'complete' and '2018-09-01' <= dttm and dttm < '2018-10-01'),
+    l as(select count(*) as cnt2 from p),
+ t as (select distinct(passenger_id) from fediq_team.novochahtinsk_orders_201909
+    where status = 'complete' and '2018-09-01' <= dttm and dttm < '2018-10-01'
+except
+select distinct(passenger_id) from fediq_team.novochahtinsk_orders_201909
+    where status = 'complete' and '2018-10-01' <= dttm and dttm < '2018-11-01'),
+j as (select count(*) as cnt1 from t)
+select 1.0 * cnt1 / cnt2 as dolya_ottoka, ('2018-10-01')::timestamp as month from j, l);
+
+insert into fediq_team.novochahtinsk_dolya_ottoka_201909 (with p as (select distinct(passenger_id) as passenger_id from fediq_team.novochahtinsk_orders_201909
+    where status = 'complete' and '2018-10-01' <= dttm and dttm < '2018-11-01'),
+    l as(select count(*) as cnt2 from p),
+ t as (select distinct(passenger_id) from fediq_team.novochahtinsk_orders_201909
+    where status = 'complete' and '2018-10-01' <= dttm and dttm < '2018-11-01'
+except
+select distinct(passenger_id) from fediq_team.novochahtinsk_orders_201909
+    where status = 'complete' and '2018-11-01' <= dttm and dttm < '2018-12-01'),
+j as (select count(*) as cnt1 from t)
+select 1.0 * cnt1 / cnt2 as dolya_ottoka, ('2018-11-01')::timestamp as month from j, l);
+
+insert into fediq_team.novochahtinsk_dolya_ottoka_201909 (with p as (select distinct(passenger_id) as passenger_id from fediq_team.novochahtinsk_orders_201909
+    where status = 'complete' and '2018-11-01' <= dttm and dttm < '2018-12-01'),
+    l as(select count(*) as cnt2 from p),
+ t as (select distinct(passenger_id) from fediq_team.novochahtinsk_orders_201909
+    where status = 'complete' and '2018-11-01' <= dttm and dttm < '2018-12-01'
+except
+select distinct(passenger_id) from fediq_team.novochahtinsk_orders_201909
+    where status = 'complete' and '2018-12-01' <= dttm and dttm < '2019-01-01'),
+j as (select count(*) as cnt1 from t)
+select 1.0 * cnt1 / cnt2 as dolya_ottoka, ('2018-12-01')::timestamp as month from j, l);
+
+insert into fediq_team.novochahtinsk_dolya_ottoka_201909 (with p as (select distinct(passenger_id) as passenger_id from fediq_team.novochahtinsk_orders_201909
+    where status = 'complete' and '2018-12-01' <= dttm and dttm < '2019-01-01'),
+    l as(select count(*) as cnt2 from p),
+ t as (select distinct(passenger_id) from fediq_team.novochahtinsk_orders_201909
+    where status = 'complete' and '2018-12-01' <= dttm and dttm < '2019-01-01'
+except
+select distinct(passenger_id) from fediq_team.novochahtinsk_orders_201909
+    where status = 'complete' and '2019-01-01' <= dttm and dttm < '2019-02-01'),
+j as (select count(*) as cnt1 from t)
+select 1.0 * cnt1 / cnt2 as dolya_ottoka, ('2019-01-01')::timestamp as month from j, l);
+
+insert into fediq_team.novochahtinsk_dolya_ottoka_201909 (with p as (select distinct(passenger_id) as passenger_id from fediq_team.novochahtinsk_orders_201909
+    where status = 'complete' and '2019-01-01' <= dttm and dttm < '2019-02-01'),
+    l as(select count(*) as cnt2 from p),
+ t as (select distinct(passenger_id) from fediq_team.novochahtinsk_orders_201909
+    where status = 'complete' and '2019-01-01' <= dttm and dttm < '2019-02-01'
+except
+select distinct(passenger_id) from fediq_team.novochahtinsk_orders_201909
+    where status = 'complete' and '2019-02-01' <= dttm and dttm < '2019-03-01'),
+j as (select count(*) as cnt1 from t)
+select 1.0 * cnt1 / cnt2 as dolya_ottoka, ('2019-02-01')::timestamp as month from j, l);
+
+insert into fediq_team.novochahtinsk_dolya_ottoka_201909 (with p as (select distinct(passenger_id) as passenger_id from fediq_team.novochahtinsk_orders_201909
+    where status = 'complete' and '2019-02-01' <= dttm and dttm < '2019-03-01'),
+    l as(select count(*) as cnt2 from p),
+ t as (select distinct(passenger_id) from fediq_team.novochahtinsk_orders_201909
+    where status = 'complete' and '2019-02-01' <= dttm and dttm < '2019-03-01'
+except
+select distinct(passenger_id) from fediq_team.novochahtinsk_orders_201909
+    where status = 'complete' and '2019-03-01' <= dttm and dttm < '2019-04-01'),
+j as (select count(*) as cnt1 from t)
+select 1.0 * cnt1 / cnt2 as dolya_ottoka, ('2019-03-01')::timestamp as month from j, l);
+
+insert into fediq_team.novochahtinsk_dolya_ottoka_201909 (with p as (select distinct(passenger_id) as passenger_id from fediq_team.novochahtinsk_orders_201909
+    where status = 'complete' and '2019-03-01' <= dttm and dttm < '2019-04-01'),
+    l as(select count(*) as cnt2 from p),
+ t as (select distinct(passenger_id) from fediq_team.novochahtinsk_orders_201909
+    where status = 'complete' and '2019-03-01' <= dttm and dttm < '2019-04-01'
+except
+select distinct(passenger_id) from fediq_team.novochahtinsk_orders_201909
+    where status = 'complete' and '2019-04-01' <= dttm and dttm < '2019-05-01'),
+j as (select count(*) as cnt1 from t)
+select 1.0 * cnt1 / cnt2 as dolya_ottoka, ('2019-04-01')::timestamp as month from j, l);
+
+insert into fediq_team.novochahtinsk_dolya_ottoka_201909 (with p as (select distinct(passenger_id) as passenger_id from fediq_team.novochahtinsk_orders_201909
+    where status = 'complete' and '2019-04-01' <= dttm and dttm < '2019-05-01'),
+    l as(select count(*) as cnt2 from p),
+ t as (select distinct(passenger_id) from fediq_team.novochahtinsk_orders_201909
+    where status = 'complete' and '2019-04-01' <= dttm and dttm < '2019-05-01'
+except
+select distinct(passenger_id) from fediq_team.novochahtinsk_orders_201909
+    where status = 'complete' and '2019-05-01' <= dttm and dttm < '2019-06-01'),
+j as (select count(*) as cnt1 from t)
+select 1.0 * cnt1 / cnt2 as dolya_ottoka, ('2019-05-01')::timestamp as month from j, l)
+
+insert into fediq_team.novochahtinsk_dolya_ottoka_201909 (with p as (select distinct(passenger_id) as passenger_id from fediq_team.novochahtinsk_orders_201909
+    where status = 'complete' and '2019-05-01' <= dttm and dttm < '2019-06-01'),
+    l as(select count(*) as cnt2 from p),
+ t as (select distinct(passenger_id) from fediq_team.novochahtinsk_orders_201909
+    where status = 'complete' and '2019-05-01' <= dttm and dttm < '2019-06-01'
+except
+select distinct(passenger_id) from fediq_team.novochahtinsk_orders_201909
+    where status = 'complete' and '2019-06-01' <= dttm and dttm < '2019-07-01'),
+j as (select count(*) as cnt1 from t)
+select 1.0 * cnt1 / cnt2 as dolya_ottoka, ('2019-06-01')::timestamp as month from j, l)
+
+insert into fediq_team.novochahtinsk_dolya_ottoka_201909 (with p as (select distinct(passenger_id) as passenger_id from fediq_team.novochahtinsk_orders_201909
+    where status = 'complete' and '2019-06-01' <= dttm and dttm < '2019-07-01'),
+    l as(select count(*) as cnt2 from p),
+ t as (select distinct(passenger_id) from fediq_team.novochahtinsk_orders_201909
+    where status = 'complete' and '2019-06-01' <= dttm and dttm < '2019-07-01'
+except
+select distinct(passenger_id) from fediq_team.novochahtinsk_orders_201909
+    where status = 'complete' and '2019-07-01' <= dttm and dttm < '2019-08-01'),
+j as (select count(*) as cnt1 from t)
+select 1.0 * cnt1 / cnt2 as dolya_ottoka, ('2019-07-01')::timestamp as month from j, l);
+
+insert into fediq_team.novochahtinsk_dolya_ottoka_201909 (with p as (select distinct(passenger_id) as passenger_id from fediq_team.novochahtinsk_orders_201909
+    where status = 'complete' and '2019-07-01' <= dttm and dttm < '2019-08-01'),
+    l as(select count(*) as cnt2 from p),
+ t as (select distinct(passenger_id) from fediq_team.novochahtinsk_orders_201909
+    where status = 'complete' and '2019-07-01' <= dttm and dttm < '2019-08-01'
+except
+select distinct(passenger_id) from fediq_team.novochahtinsk_orders_201909
+    where status = 'complete' and '2019-08-01' <= dttm and dttm < '2019-09-01'),
+j as (select count(*) as cnt1 from t)
+select 1.0 * cnt1 / cnt2 as dolya_ottoka, ('2019-08-01')::timestamp as month from j, l);
+
+insert into fediq_team.novochahtinsk_dolya_ottoka_201909 (with p as (select distinct(passenger_id) as passenger_id from fediq_team.novochahtinsk_orders_201909
+    where status = 'complete' and '2019-08-01' <= dttm and dttm < '2019-09-01'),
+    l as(select count(*) as cnt2 from p),
+ t as (select distinct(passenger_id) from fediq_team.novochahtinsk_orders_201909
+    where status = 'complete' and '2019-08-01' <= dttm and dttm < '2019-09-01'
+except
+select distinct(passenger_id) from fediq_team.novochahtinsk_orders_201909
+    where status = 'complete' and '2019-09-01' <= dttm and dttm < '2019-10-01'),
+j as (select count(*) as cnt1 from t)
+select 1.0 * cnt1 / cnt2 as dolya_ottoka, ('2019-09-01')::timestamp as month from j, l);
