34f7f7af579d99c05e37c354c1529211fbf2830d
Yaroslav <Yaroslaw.serow@yandex.ru>
2020-11-24 02:20:53 +0300

    Create nijnii_tagil_mean_check_201909.sql

---
+++ calculation_of_metrics/metrics/nijnii_tagil_mean_check_201909.sql
@@ -0,0 +1,169 @@
+create table fediq_team.nijnii_tagil_mean_check_201909 as (
+    SELECT CAST(min(dttm) AS DATE), car_class, round(sum(order_cost) / count(order_id)) as mean_check, count(order_id)
+    FROM fediq_team.nijnii_tagil_orders_201909
+    WHERE status = 'complete'
+      and dttm IS NOT NULL
+      and '2018_01_01' <= dttm
+      and dttm <= '2018_02_01'
+    GROUP BY car_class
+    UNION
+    SELECT CAST(min(dttm) AS DATE), car_class, round(sum(order_cost) / count(order_id)) as mean_check, count(order_id)
+    FROM fediq_team.nijnii_tagil_orders_201909
+    WHERE status = 'complete'
+      and dttm IS NOT NULL
+      and '2018_02_01' <= dttm
+      and dttm <= '2018_03_01'
+    GROUP BY car_class
+    UNION
+    SELECT CAST(min(dttm) AS DATE), car_class, round(sum(order_cost) / count(order_id)) as mean_check, count(order_id)
+    FROM fediq_team.nijnii_tagil_orders_201909
+    WHERE status = 'complete'
+      and dttm IS NOT NULL
+      and '2018_03_01' <= dttm
+      and dttm <= '2018_04_01'
+    GROUP BY car_class
+    UNION
+    SELECT CAST(min(dttm) AS DATE), car_class, round(sum(order_cost) / count(order_id)) as mean_check, count(order_id)
+    FROM fediq_team.nijnii_tagil_orders_201909
+    WHERE status = 'complete'
+      and dttm IS NOT NULL
+      and '2018_04_01' <= dttm
+      and dttm <= '2018_05_01'
+    GROUP BY car_class
+    UNION
+    SELECT CAST(min(dttm) AS DATE), car_class, round(sum(order_cost) / count(order_id)) as mean_check, count(order_id)
+    FROM fediq_team.nijnii_tagil_orders_201909
+    WHERE status = 'complete'
+      and dttm IS NOT NULL
+      and '2018_05_01' <= dttm
+      and dttm <= '2018_06_01'
+    GROUP BY car_class
+    UNION
+    SELECT CAST(min(dttm) AS DATE), car_class, round(sum(order_cost) / count(order_id)) as mean_check, count(order_id)
+    FROM fediq_team.nijnii_tagil_orders_201909
+    WHERE status = 'complete'
+      and dttm IS NOT NULL
+      and '2018_06_01' <= dttm
+      and dttm <= '2018_07_01'
+    GROUP BY car_class
+    UNION
+    SELECT CAST(min(dttm) AS DATE), car_class, round(sum(order_cost) / count(order_id)) as mean_check, count(order_id)
+    FROM fediq_team.nijnii_tagil_orders_201909
+    WHERE status = 'complete'
+      and dttm IS NOT NULL
+      and '2018_07_01' <= dttm
+      and dttm <= '2018_08_01'
+    GROUP BY car_class
+    UNION
+    SELECT CAST(min(dttm) AS DATE), car_class, round(sum(order_cost) / count(order_id)) as mean_check, count(order_id)
+    FROM fediq_team.nijnii_tagil_orders_201909
+    WHERE status = 'complete'
+      and dttm IS NOT NULL
+      and '2018_08_01' <= dttm
+      and dttm <= '2018_09_01'
+    GROUP BY car_class
+    UNION
+    SELECT CAST(min(dttm) AS DATE), car_class, round(sum(order_cost) / count(order_id)) as mean_check, count(order_id)
+    FROM fediq_team.nijnii_tagil_orders_201909
+    WHERE status = 'complete'
+      and dttm IS NOT NULL
+      and '2018_09_01' <= dttm
+      and dttm <= '2018_10_01'
+    GROUP BY car_class
+    UNION
+    SELECT CAST(min(dttm) AS DATE), car_class, round(sum(order_cost) / count(order_id)) as mean_check, count(order_id)
+    FROM fediq_team.nijnii_tagil_orders_201909
+    WHERE status = 'complete'
+      and dttm IS NOT NULL
+      and '2018_10_01' <= dttm
+      and dttm <= '2018_11_01'
+    GROUP BY car_class
+    UNION
+    SELECT CAST(min(dttm) AS DATE), car_class, round(sum(order_cost) / count(order_id)) as mean_check, count(order_id)
+    FROM fediq_team.nijnii_tagil_orders_201909
+    WHERE status = 'complete'
+      and dttm IS NOT NULL
+      and '2018_11_01' <= dttm
+      and dttm <= '2018_12_01'
+    GROUP BY car_class
+    UNION
+    SELECT CAST(min(dttm) AS DATE), car_class, round(sum(order_cost) / count(order_id)) as mean_check, count(order_id)
+    FROM fediq_team.nijnii_tagil_orders_201909
+    WHERE status = 'complete'
+      and dttm IS NOT NULL
+      and '2018_12_01' <= dttm
+      and dttm <= '2019_01_01'
+    GROUP BY car_class
+    UNION
+    SELECT CAST(min(dttm) AS DATE), car_class, round(sum(order_cost) / count(order_id)) as mean_check, count(order_id)
+    FROM fediq_team.nijnii_tagil_orders_201909
+    WHERE status = 'complete'
+      and dttm IS NOT NULL
+      and '2019_01_01' <= dttm
+      and dttm <= '2019_02_01'
+    GROUP BY car_class
+    UNION
+    SELECT CAST(min(dttm) AS DATE), car_class, round(sum(order_cost) / count(order_id)) as mean_check, count(order_id)
+    FROM fediq_team.nijnii_tagil_orders_201909
+    WHERE status = 'complete'
+      and dttm IS NOT NULL
+      and '2019_02_01' <= dttm
+      and dttm <= '2019_03_01'
+    GROUP BY car_class
+    UNION
+    SELECT CAST(min(dttm) AS DATE), car_class, round(sum(order_cost) / count(order_id)) as mean_check, count(order_id)
+    FROM fediq_team.nijnii_tagil_orders_201909
+    WHERE status = 'complete'
+      and dttm IS NOT NULL
+      and '2019_03_01' <= dttm
+      and dttm <= '2019_04_01'
+    GROUP BY car_class
+    UNION
+    SELECT CAST(min(dttm) AS DATE), car_class, round(sum(order_cost) / count(order_id)) as mean_check, count(order_id)
+    FROM fediq_team.nijnii_tagil_orders_201909
+    WHERE status = 'complete'
+      and dttm IS NOT NULL
+      and '2019_04_01' <= dttm
+      and dttm <= '2019_05_01'
+    GROUP BY car_class
+    UNION
+    SELECT CAST(min(dttm) AS DATE), car_class, round(sum(order_cost) / count(order_id)) as mean_check, count(order_id)
+    FROM fediq_team.nijnii_tagil_orders_201909
+    WHERE status = 'complete'
+      and dttm IS NOT NULL
+      and '2019_05_01' <= dttm
+      and dttm <= '2019_06_01'
+    GROUP BY car_class
+    UNION
+    SELECT CAST(min(dttm) AS DATE), car_class, round(sum(order_cost) / count(order_id)) as mean_check, count(order_id)
+    FROM fediq_team.nijnii_tagil_orders_201909
+    WHERE status = 'complete'
+      and dttm IS NOT NULL
+      and '2019_06_01' <= dttm
+      and dttm <= '2019_07_01'
+    GROUP BY car_class
+    UNION
+    SELECT CAST(min(dttm) AS DATE), car_class, round(sum(order_cost) / count(order_id)) as mean_check, count(order_id)
+    FROM fediq_team.nijnii_tagil_orders_201909
+    WHERE status = 'complete'
+      and dttm IS NOT NULL
+      and '2019_07_01' <= dttm
+      and dttm <= '2019_08_01'
+    GROUP BY car_class
+    UNION
+    SELECT CAST(min(dttm) AS DATE), car_class, round(sum(order_cost) / count(order_id)) as mean_check, count(order_id)
+    FROM fediq_team.nijnii_tagil_orders_201909
+    WHERE status = 'complete'
+      and dttm IS NOT NULL
+      and '2019_08_01' <= dttm
+      and dttm <= '2019_09_01'
+    GROUP BY car_class
+    UNION
+    SELECT CAST(min(dttm) AS DATE), car_class, round(sum(order_cost) / count(order_id)) as mean_check, count(order_id)
+    FROM fediq_team.nijnii_tagil_orders_201909
+    WHERE status = 'complete'
+      and dttm IS NOT NULL
+      and '2019_09_01' <= dttm
+      and dttm <= '2019_10_01'
+    GROUP BY car_class
+);
