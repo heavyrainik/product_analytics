30642464bb19c19045bd31ad95add9d23140887c
Yaroslav <Yaroslaw.serow@yandex.ru>
2020-11-24 02:32:42 +0300

    Create novochahtinsk_active_10_201909.sql

---
+++ calculation_of_metrics/metrics/novochahtinsk_active_10_201909.sql
@@ -0,0 +1,169 @@
+create table fediq_team.novochahtinsk_active_10_201909 as (
+    SELECT CAST(min(month) AS DATE), count(driver_id)
+    FROM (SELECT min(dttm) as month, driver_id, count(order_id) as amount_of_orders
+          FROM fediq_team.novochahtinsk_orders_201909
+          WHERE '2018_01_01' <= dttm
+            and dttm <= '2018_02_01'
+          GROUP BY driver_id) as A
+    WHERE amount_of_orders > 9
+    UNION
+    SELECT CAST(min(month) AS DATE), count(driver_id)
+    FROM (SELECT min(dttm) as month, driver_id, count(order_id) as amount_of_orders
+          FROM fediq_team.novochahtinsk_orders_201909
+          WHERE '2018_02_01' <= dttm
+            and dttm <= '2018_03_01'
+          GROUP BY driver_id) as A
+    WHERE amount_of_orders > 9
+    UNION
+    SELECT CAST(min(month) AS DATE), count(driver_id)
+    FROM (SELECT min(dttm) as month, driver_id, count(order_id) as amount_of_orders
+          FROM fediq_team.novochahtinsk_orders_201909
+          WHERE '2018_03_01' <= dttm
+            and dttm <= '2018_04_01'
+          GROUP BY driver_id) as A
+    WHERE amount_of_orders > 9
+    UNION
+    SELECT CAST(min(month) AS DATE), count(driver_id)
+    FROM (SELECT min(dttm) as month, driver_id, count(order_id) as amount_of_orders
+          FROM fediq_team.novochahtinsk_orders_201909
+          WHERE '2018_04_01' <= dttm
+            and dttm <= '2018_05_01'
+          GROUP BY driver_id) as A
+    WHERE amount_of_orders > 9
+    UNION
+    SELECT CAST(min(month) AS DATE), count(driver_id)
+    FROM (SELECT min(dttm) as month, driver_id, count(order_id) as amount_of_orders
+          FROM fediq_team.novochahtinsk_orders_201909
+          WHERE '2018_05_01' <= dttm
+            and dttm <= '2018_06_01'
+          GROUP BY driver_id) as A
+    WHERE amount_of_orders > 9
+    UNION
+    SELECT CAST(min(month) AS DATE), count(driver_id)
+    FROM (SELECT min(dttm) as month, driver_id, count(order_id) as amount_of_orders
+          FROM fediq_team.novochahtinsk_orders_201909
+          WHERE '2018_06_01' <= dttm
+            and dttm <= '2018_07_01'
+          GROUP BY driver_id) as A
+    WHERE amount_of_orders > 9
+    UNION
+    SELECT CAST(min(month) AS DATE), count(driver_id)
+    FROM (SELECT min(dttm) as month, driver_id, count(order_id) as amount_of_orders
+          FROM fediq_team.novochahtinsk_orders_201909
+          WHERE '2018_07_01' <= dttm
+            and dttm <= '2018_08_01'
+          GROUP BY driver_id) as A
+    WHERE amount_of_orders > 9
+    UNION
+    SELECT CAST(min(month) AS DATE), count(driver_id)
+    FROM (SELECT min(dttm) as month, driver_id, count(order_id) as amount_of_orders
+          FROM fediq_team.novochahtinsk_orders_201909
+          WHERE '2018_08_01' <= dttm
+            and dttm <= '2018_09_01'
+          GROUP BY driver_id) as A
+    WHERE amount_of_orders > 9
+    UNION
+    SELECT CAST(min(month) AS DATE), count(driver_id)
+    FROM (SELECT min(dttm) as month, driver_id, count(order_id) as amount_of_orders
+          FROM fediq_team.novochahtinsk_orders_201909
+          WHERE '2018_09_01' <= dttm
+            and dttm <= '2018_10_01'
+          GROUP BY driver_id) as A
+    WHERE amount_of_orders > 9
+    UNION
+    SELECT CAST(min(month) AS DATE), count(driver_id)
+    FROM (SELECT min(dttm) as month, driver_id, count(order_id) as amount_of_orders
+          FROM fediq_team.novochahtinsk_orders_201909
+          WHERE '2018_10_01' <= dttm
+            and dttm <= '2018_11_01'
+          GROUP BY driver_id) as A
+    WHERE amount_of_orders > 9
+    UNION
+    SELECT CAST(min(month) AS DATE), count(driver_id)
+    FROM (SELECT min(dttm) as month, driver_id, count(order_id) as amount_of_orders
+          FROM fediq_team.novochahtinsk_orders_201909
+          WHERE '2018_11_01' <= dttm
+            and dttm <= '2018_12_01'
+          GROUP BY driver_id) as A
+    WHERE amount_of_orders > 9
+    UNION
+    SELECT CAST(min(month) AS DATE), count(driver_id)
+    FROM (SELECT min(dttm) as month, driver_id, count(order_id) as amount_of_orders
+          FROM fediq_team.novochahtinsk_orders_201909
+          WHERE '2018_12_01' <= dttm
+            and dttm <= '2019_01_01'
+          GROUP BY driver_id) as A
+    WHERE amount_of_orders > 9
+    UNION
+    SELECT CAST(min(month) AS DATE), count(driver_id)
+    FROM (SELECT min(dttm) as month, driver_id, count(order_id) as amount_of_orders
+          FROM fediq_team.novochahtinsk_orders_201909
+          WHERE '2019_01_01' <= dttm
+            and dttm <= '2019_02_01'
+          GROUP BY driver_id) as A
+    WHERE amount_of_orders > 9
+    UNION
+    SELECT CAST(min(month) AS DATE), count(driver_id)
+    FROM (SELECT min(dttm) as month, driver_id, count(order_id) as amount_of_orders
+          FROM fediq_team.novochahtinsk_orders_201909
+          WHERE '2019_02_01' <= dttm
+            and dttm <= '2019_03_01'
+          GROUP BY driver_id) as A
+    WHERE amount_of_orders > 9
+    UNION
+    SELECT CAST(min(month) AS DATE), count(driver_id)
+    FROM (SELECT min(dttm) as month, driver_id, count(order_id) as amount_of_orders
+          FROM fediq_team.novochahtinsk_orders_201909
+          WHERE '2019_03_01' <= dttm
+            and dttm <= '2019_04_01'
+          GROUP BY driver_id) as A
+    WHERE amount_of_orders > 9
+    UNION
+    SELECT CAST(min(month) AS DATE), count(driver_id)
+    FROM (SELECT min(dttm) as month, driver_id, count(order_id) as amount_of_orders
+          FROM fediq_team.novochahtinsk_orders_201909
+          WHERE '2019_04_01' <= dttm
+            and dttm <= '2019_05_01'
+          GROUP BY driver_id) as A
+    WHERE amount_of_orders > 9
+    UNION
+    SELECT CAST(min(month) AS DATE), count(driver_id)
+    FROM (SELECT min(dttm) as month, driver_id, count(order_id) as amount_of_orders
+          FROM fediq_team.novochahtinsk_orders_201909
+          WHERE '2019_05_01' <= dttm
+            and dttm <= '2019_06_01'
+          GROUP BY driver_id) as A
+    WHERE amount_of_orders > 9
+    UNION
+    SELECT CAST(min(month) AS DATE), count(driver_id)
+    FROM (SELECT min(dttm) as month, driver_id, count(order_id) as amount_of_orders
+          FROM fediq_team.novochahtinsk_orders_201909
+          WHERE '2019_06_01' <= dttm
+            and dttm <= '2019_07_01'
+          GROUP BY driver_id) as A
+    WHERE amount_of_orders > 9
+    UNION
+    SELECT CAST(min(month) AS DATE), count(driver_id)
+    FROM (SELECT min(dttm) as month, driver_id, count(order_id) as amount_of_orders
+          FROM fediq_team.novochahtinsk_orders_201909
+          WHERE '2019_07_01' <= dttm
+            and dttm <= '2019_08_01'
+          GROUP BY driver_id) as A
+    WHERE amount_of_orders > 9
+    UNION
+    SELECT CAST(min(month) AS DATE), count(driver_id)
+    FROM (SELECT min(dttm) as month, driver_id, count(order_id) as amount_of_orders
+          FROM fediq_team.novochahtinsk_orders_201909
+          WHERE '2019_08_01' <= dttm
+            and dttm <= '2019_09_01'
+          GROUP BY driver_id) as A
+    WHERE amount_of_orders > 9
+    UNION
+    SELECT CAST(min(month) AS DATE), count(driver_id)
+    FROM (SELECT min(dttm) as month, driver_id, count(order_id) as amount_of_orders
+          FROM fediq_team.novochahtinsk_orders_201909
+          WHERE '2019_09_01' <= dttm
+            and dttm <= '2019_10_01'
+          GROUP BY driver_id) as A
+    WHERE amount_of_orders > 9
+);
