a8529cf3b7ff65e17ca20da8240631b76ccef57a
Yaroslav <Yaroslaw.serow@yandex.ru>
2020-11-24 02:29:39 +0300

    Create novochahtinsk_drivers_count_201909.sql

---
+++ calculation_of_metrics/metrics/novochahtinsk_drivers_count_201909.sql
@@ -0,0 +1,106 @@
+create table fediq_team.novochahtinsk_drivers_count_201909 as (
+    SELECT CAST(min(dttm) AS DATE), count(DISTINCT driver_id)
+    FROM fediq_team.novochahtinsk_orders_201909
+    WHERE '2018_01_01' <= dttm
+      and dttm <= '2018_02_01'
+    UNION
+    SELECT CAST(min(dttm) AS DATE), count(DISTINCT driver_id)
+    FROM fediq_team.novochahtinsk_orders_201909
+    WHERE '2018_02_01' <= dttm
+      and dttm <= '2018_03_01'
+    UNION
+    SELECT CAST(min(dttm) AS DATE), count(DISTINCT driver_id)
+    FROM fediq_team.novochahtinsk_orders_201909
+    WHERE '2018_03_01' <= dttm
+      and dttm <= '2018_04_01'
+    UNION
+    SELECT CAST(min(dttm) AS DATE), count(DISTINCT driver_id)
+    FROM fediq_team.novochahtinsk_orders_201909
+    WHERE '2018_04_01' <= dttm
+      and dttm <= '2018_05_01'
+    UNION
+    SELECT CAST(min(dttm) AS DATE), count(DISTINCT driver_id)
+    FROM fediq_team.novochahtinsk_orders_201909
+    WHERE '2018_05_01' <= dttm
+      and dttm <= '2018_06_01'
+    UNION
+    SELECT CAST(min(dttm) AS DATE), count(DISTINCT driver_id)
+    FROM fediq_team.novochahtinsk_orders_201909
+    WHERE '2018_06_01' <= dttm
+      and dttm <= '2018_07_01'
+    UNION
+    SELECT CAST(min(dttm) AS DATE), count(DISTINCT driver_id)
+    FROM fediq_team.novochahtinsk_orders_201909
+    WHERE '2018_07_01' <= dttm
+      and dttm <= '2018_08_01'
+    UNION
+    SELECT CAST(min(dttm) AS DATE), count(DISTINCT driver_id)
+    FROM fediq_team.novochahtinsk_orders_201909
+    WHERE '2018_08_01' <= dttm
+      and dttm <= '2018_09_01'
+    UNION
+    SELECT CAST(min(dttm) AS DATE), count(DISTINCT driver_id)
+    FROM fediq_team.novochahtinsk_orders_201909
+    WHERE '2018_09_01' <= dttm
+      and dttm <= '2018_10_01'
+    UNION
+    SELECT CAST(min(dttm) AS DATE), count(DISTINCT driver_id)
+    FROM fediq_team.novochahtinsk_orders_201909
+    WHERE '2018_10_01' <= dttm
+      and dttm <= '2018_11_01'
+    UNION
+    SELECT CAST(min(dttm) AS DATE), count(DISTINCT driver_id)
+    FROM fediq_team.novochahtinsk_orders_201909
+    WHERE '2018_11_01' <= dttm
+      and dttm <= '2018_12_01'
+    UNION
+    SELECT CAST(min(dttm) AS DATE), count(DISTINCT driver_id)
+    FROM fediq_team.novochahtinsk_orders_201909
+    WHERE '2018_12_01' <= dttm
+      and dttm <= '2019_01_01'
+    UNION
+    SELECT CAST(min(dttm) AS DATE), count(DISTINCT driver_id)
+    FROM fediq_team.novochahtinsk_orders_201909
+    WHERE '2019_01_01' <= dttm
+      and dttm <= '2019_02_01'
+    UNION
+    SELECT CAST(min(dttm) AS DATE), count(DISTINCT driver_id)
+    FROM fediq_team.novochahtinsk_orders_201909
+    WHERE '2019_02_01' <= dttm
+      and dttm <= '2019_03_01'
+    UNION
+    SELECT CAST(min(dttm) AS DATE), count(DISTINCT driver_id)
+    FROM fediq_team.novochahtinsk_orders_201909
+    WHERE '2019_03_01' <= dttm
+      and dttm <= '2019_04_01'
+    UNION
+    SELECT CAST(min(dttm) AS DATE), count(DISTINCT driver_id)
+    FROM fediq_team.novochahtinsk_orders_201909
+    WHERE '2019_04_01' <= dttm
+      and dttm <= '2019_05_01'
+    UNION
+    SELECT CAST(min(dttm) AS DATE), count(DISTINCT driver_id)
+    FROM fediq_team.novochahtinsk_orders_201909
+    WHERE '2019_05_01' <= dttm
+      and dttm <= '2019_06_01'
+    UNION
+    SELECT CAST(min(dttm) AS DATE), count(DISTINCT driver_id)
+    FROM fediq_team.novochahtinsk_orders_201909
+    WHERE '2019_06_01' <= dttm
+      and dttm <= '2019_07_01'
+    UNION
+    SELECT CAST(min(dttm) AS DATE), count(DISTINCT driver_id)
+    FROM fediq_team.novochahtinsk_orders_201909
+    WHERE '2019_07_01' <= dttm
+      and dttm <= '2019_08_01'
+    UNION
+    SELECT CAST(min(dttm) AS DATE), count(DISTINCT driver_id)
+    FROM fediq_team.novochahtinsk_orders_201909
+    WHERE '2019_08_01' <= dttm
+      and dttm <= '2019_09_01'
+    UNION
+    SELECT CAST(min(dttm) AS DATE), count(DISTINCT driver_id)
+    FROM fediq_team.novochahtinsk_orders_201909
+    WHERE '2019_09_01' <= dttm
+      and dttm <= '2019_10_01'
+);
