c445869f3d2ccdbb795e1a97ce3845c9431ddbab
heavyrainik <67919163+heavyrainik@users.noreply.github.com>
2020-12-03 20:55:13 +0300

    Delete nonovochahtinsk_mau_201909.sql

--- calculation_of_metrics/metrics/nonovochahtinsk_mau_201909.sql
+++
@@ -1,5 +0,0 @@
-create table fediq_team.novochahtinsk_mau_201909 as(
-with p as (select passenger_id, date_trunc('month', dttm) as month from fediq_team.novochahtinsk_orders_201909 where status = 'complete')
-select count(distinct passenger_id) as MAU, month
-from p
-group by(month));
