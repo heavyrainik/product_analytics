8b7b044e62bf01a655a88ab49dc6e6b7c7bc6834
heavyrainik <67919163+heavyrainik@users.noreply.github.com>
2020-12-03 20:57:07 +0300

    Delete novoshakhtinsk_rettention_2019_09.sql

--- calculation_of_metrics/metrics/novoshakhtinsk_rettention_2019_09.sql
+++
@@ -1,7 +0,0 @@
-create table fediq_team.novoshakhtinsk_rettention_2019_09 as(with sum_of_dau as(
-with p as (select dau, date_trunc('month', day) as month from fediq_team.Nijnii_tagil_dau_201909)
-select sum(dau) as sum_of_dau, month
-from p
-group by(month))
-    select 1.0*sum_of_dau / mau as rettention, fediq_team.novochahtinsk_mau_201909.month as month from sum_of_dau, fediq_team.novochahtinsk_mau_201909
-where sum_of_dau.month = fediq_team.novochahtinsk_mau_201909.month)
