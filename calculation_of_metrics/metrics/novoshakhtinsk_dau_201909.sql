5c87e903761adb5c3814c2089b5f895037578ab4
heavyrainik <67919163+heavyrainik@users.noreply.github.com>
2020-12-03 20:56:20 +0300

    Delete novoshakhtinsk_dau_201909.sql

--- calculation_of_metrics/metrics/novoshakhtinsk_dau_201909.sql
+++
@@ -1,5 +0,0 @@
-create table fediq_team.novoshakhtinsk_dau_201909 as(
-with p as (select passenger_id, date_trunc('day', dttm) as day from fediq_team.novochahtinsk_orders_201909 where status='complete')
-select count(distinct passenger_id) as DAU, day
-from p
-group by(day));
