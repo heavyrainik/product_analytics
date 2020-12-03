f238c45b2b2d2686d0d36076440275846713f6a9
heavyrainik <67919163+heavyrainik@users.noreply.github.com>
2020-12-03 20:56:13 +0300

    Delete novochahtinsk_old_client_201909.sql

--- calculation_of_metrics/metrics/novochahtinsk_old_client_201909.sql
+++
@@ -1,5 +0,0 @@
-create table fediq_team.novochahtinsk_old_client_201909 as(
-    SELECT month, mau - count as old_pay
-    FROM fediq_team.novochahtinsk_mau_201909
-    INNER JOIN fediq_team.novochahtinsk_new_client_201909 ON month = timestamp
-);
